04ef52d532cd7dd85406e632a25a3a3cb41fcb50
Erasmo <e.salsano@ifortech.com>
2025-12-02 16:07:44 +0100

    Fix assegnazione automatica ticket. Ora non modifica entrambi i valori, ma solo quello da modificare.

--- app/Jobs/AutoAssignTicket.php
+++ app/Jobs/AutoAssignTicket.php
@@ -52,62 +52,67 @@ class AutoAssignTicket implements ShouldQueue
 
             $groups = $ticket->ticketType->groups()->get();
 
-            $selectedGroup = null;
-            $adminUser = null;
+            $selectedGroup = $ticket->group ?? null;
+            $adminUser = $ticket->handler ?? null;
 
-            if ($groups->count() == 0) {
-                // Invia mail di avviso che un ticket non ha un gruppo associato
-                continue;
-            }
-            
-            if($ticket->group && $ticket->group->users->count() > 0){
-                // Se il ticket ha già un gruppo associato e quel gruppo ha utenti, usa quello
-                $selectedGroup = $ticket->group;
-                $adminUser = $selectedGroup->users->first();
-            }else{
-                // Altrimenti cerca il primo gruppo con utenti
-                foreach ($groups as $group) {
-                    if ($group->users->count() > 0) {
-                        $adminUser = $group->users->first();
-                        $selectedGroup = $group;
-                        break;
+            // Se non ha il gestore lo assegna automaticamente, altrimenti lascia quello che c'è.
+            if(!$adminUser){
+                if ($groups->count() == 0) {
+                    // Invia mail di avviso che un ticket non ha un gruppo associato
+                    continue;
+                }
+                
+                if($ticket->group && $ticket->group->users->count() > 0){
+                    // Se il ticket ha già un gruppo associato e quel gruppo ha utenti, usa quello
+                    $selectedGroup = $ticket->group;
+                    $adminUser = $selectedGroup->users->first();
+                }else{
+                    // Altrimenti cerca il primo gruppo con utenti
+                    foreach ($groups as $group) {
+                        if ($group->users->count() > 0) {
+                            $adminUser = $group->users->first();
+                            $selectedGroup = $group;
+                            break;
+                        }
                     }
                 }
+                if (! $adminUser) {
+                    // Invia mail di avviso che un ticket non ha un utente associato
+                    continue;
+                }
+    
+                // Assegna il ticket all'utente
+                $ticket->update([
+                    'admin_user_id' => $adminUser->id,
+                ]);
+    
+                $assignUpdate = TicketStatusUpdate::create([
+                    'ticket_id' => $ticket->id,
+                    'user_id' => $adminUser->id, // Anche se non l'ha richiesto lui, ma è obbligatorio questo campo e deve essere un utente esistente
+                    'content' => "Modifica automatica: Ticket assegnato all'utente ".$adminUser->name.' '.($adminUser->surname ?? ''),
+                    'type' => 'assign',
+                ]);
+
+                dispatch(new SendUpdateEmail($assignUpdate, true));
+                // Invia mail di avviso a tutto il gruppo che un ticket è stato assegnato automaticamente e non è detto che venga gestito da quell'utente.
+                dispatch(new SendGroupWarningEmail('auto-assign', $selectedGroup, $ticket, $assignUpdate));
             }
-            if (! $adminUser) {
-                // Invia mail di avviso che un ticket non ha un utente associato
-                continue;
+
+            // Si controlla se lo stato è "Nuovo" e in tal caso lo si porta a "Assegnato"
+            if ($ticket->stage_id == $newTicketStageId) {
+                $assignedStageId = TicketStage::where('system_key', 'assigned')->value('id');
+                $ticket->update(['stage_id' => $assignedStageId]);
+                $newStageText = TicketStage::find($assignedStageId)->name;
+    
+                $statusUpdate = TicketStatusUpdate::create([
+                    'ticket_id' => $ticket->id,
+                    'user_id' => $adminUser->id,
+                    'old_stage_id' => $newTicketStageId,
+                    'new_stage_id' => $assignedStageId,
+                    'content' => 'Modifica automatica: Stato del ticket modificato in "'.$newStageText.'"',
+                    'type' => 'status',
+                ]);
             }
-
-            // Assegna il ticket all'utente
-            $ticket->update([
-                'admin_user_id' => $adminUser->id,
-            ]);
-
-            $assignUpdate = TicketStatusUpdate::create([
-                'ticket_id' => $ticket->id,
-                'user_id' => $adminUser->id, // Anche se non l'ha richiesto lui, ma è obbligatorio questo campo e deve essere un utente esistente
-                'content' => "Modifica automatica: Ticket assegnato all'utente ".$adminUser->name.' '.($adminUser->surname ?? ''),
-                'type' => 'assign',
-            ]);
-
-            // Se lo stato è 'Nuovo' aggiornarlo in assegnato. Lo stato già sappiamo che è 0, quindi non serve il controllo
-            $assignedStageId = TicketStage::where('system_key', 'assigned')->value('id');
-            $ticket->update(['stage_id' => $assignedStageId]);
-            $newStageText = TicketStage::find($assignedStageId)->name;
-
-            $statusUpdate = TicketStatusUpdate::create([
-                'ticket_id' => $ticket->id,
-                'user_id' => $adminUser->id,
-                'old_stage_id' => $newTicketStageId,
-                'new_stage_id' => $assignedStageId,
-                'content' => 'Modifica automatica: Stato del ticket modificato in "'.$newStageText.'"',
-                'type' => 'status',
-            ]);
-
-            dispatch(new SendUpdateEmail($assignUpdate, true));
-            // Invia mail di avviso a tutto il gruppo che un ticket è stato assegnato automaticamente e non è detto che venga gestito da quell'utente.
-            dispatch(new SendGroupWarningEmail('auto-assign', $selectedGroup, $ticket, $assignUpdate));
 
             // Invalida la cache per chi ha creato il ticket e per i referenti.
             $ticket->invalidateCache();
